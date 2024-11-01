ba7cd2ba79099b1c5fde0c40ac8c94fba4c8746d
limjh <limjh5866@gmail.com>
2024-11-02 03:44:45 +0900

    실수로도 연산 가능하게 수정, 예외처리 추가 및 수정, 로직 수정 및 완성

--- calculator.py
+++ calculator.py
@@ -1,32 +1,36 @@
 # [ 계산기 프로그램 ]
 
 # 덧셈
-def plus(x, answer):
-    answer += x
-    return answer
+def plus(x, total):
+    total += x
+    return total
 
 # 뺼셈
-def minus(x, answer):
-    answer -= x
-    return answer
+def minus(x, total):
+    total -= x
+    return total
 
 # 곱하기
-def time(x, answer):
-    answer *= x
-    return answer
+def time(x, total):
+    total *= x
+    return total
 
 # 나누기
-def div(x, answer):
-    answer /= x
-    return answer
+def div(x, total):
+    if x == 0:
+        print("0으로 나눌 수 없습니다.")
+        return total
+    total /= x
+    return total
 
 # 나머지
-def rest(x, answer):
-    answer %= x
-    return answer
+def rest(x, total):
+    total %= x
+    return total
 
 # [ MAIN CODE ]
 
+# < 설명 >
 print("[ H A N N A M   C A L C U L A T O R ]")
 print("한남대 계산기 프로그램에 오신 것을 환영합니다.")
 print()
@@ -38,10 +42,15 @@ print("3. 연산자 입력칸에 '=' 입력시 값이 출력됩니다.")
 print("4. 연산자 입력칸에 'AC' 입력시 값이 초기화 됩니다.")
 print()
 
+# < 실행 >
 print("[ 계산기 실행 ]")
-answer = int(input("숫자를 입력해주세요 : "))
 
+# 출력 값을 알려주는 answer에 처음 숫자를 저장.
+answer = float(input("숫자를 입력해주세요 : "))
+
+# 무한 루프 ('=' 을 입력할 때 까지)
 while True:
+    # 연산자 입력
     shape = input("연산자를 입력해주세요 : ")
 
     # 무한루프 종료 조건
@@ -49,34 +58,41 @@ while True:
         print("[", answer, "]")
         break
 
-    n1 = int(input("숫자를 입력해주세요 : "))
-
-    # 예외처리
+    # 연산자 예외처리
     if shape != "=" and shape != "+" and shape != "-" and shape != "*" and shape != "/" and shape != "%" and shape != "AC":
         print("연산자가 아닙니다.")
         shape = input("연산자를 입력해주세요 : ")
- 
-    if shape == "+":
+    
+    # AC를 입력했을 때
+    if shape == "AC": 
+        answer = 0 # answer을 0으로 초기화
+        print("현재 값 :",  answer)
+        answer = float(input("숫자를 입력해주세요 : "))
+        continue
+
+    # 연산할 숫자 입력
+    n1 = float(input("숫자를 입력해주세요 : "))
+
+    # 계산 
+    if shape == "+": # 덧셈
         answer = plus(n1, answer)
         print("현재 값 :",  answer)
 
-    elif shape == "-":
+    elif shape == "-": # 뺄셈
         answer = minus(n1, answer)
         print("현재 값 :",  answer)
 
-    elif shape == "*":
+    elif shape == "*": # 곱셈
         answer = time(n1, answer)
         print("현재 값 :",  answer)
 
-    elif shape == "/":
+    elif shape == "/": # 나눗셈
         answer = div(n1, answer)
         print("현재 값 :",  answer)
 
-    elif shape == "%":
+    elif shape == "%": # 나머지
         answer = rest(n1, answer)
         print("현재 값 :",  answer)
     
-    elif shape == "AC":
-        answer = 0
 
 
