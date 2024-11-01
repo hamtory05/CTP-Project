4c1bbf461d7efd7ad08c8b6fc425b4ddca914164
limjh <limjh5866@gmail.com>
2024-11-01 23:47:48 +0900

    first commit

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# [ 계산기 프로그램 ]
+
+# 덧셈
+def puls():
+    pass
+
+# 뺼셈
+def minus():
+    pass
+
+# 곱하기
+def time():
+    pass
+
+# 나누기
+def div():
+    pass
+
+# 제곱
+def square():
+    pass
+
+# 나머지
+def rest():
+    pass
+
+answer = 0
+
