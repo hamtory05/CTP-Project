da5ec07fad7dfdeb240262b89752a5b27f5d0365
limjh <limjh5866@gmail.com>
2024-11-02 00:26:00 +0900

    덧셈, 뺄셈, 나누기, 곱셈, 나머지 함수 틀 추가 & 메인 코드 기초 틀 추가

--- calculator.py
+++ calculator.py
@@ -16,13 +16,44 @@ def time():
 def div():
     pass
 
-# 제곱
-def square():
-    pass
-
 # 나머지
 def rest():
     pass
 
+# [ MAIN CODE ]
+
+print("[ H A N N A M   C A L C U L A T O R ]")
+print("한남대 계산기 프로그램에 오신 것을 환영합니다.")
+print()
+
+print("[ 계산기 설명 ]")
+print("1. 연산자는 '+, -, *, /, %' 만 존재합니다.")
+print("2. 잘못된 연산자 입력시 다시 연산자를 입력할 수 있습니다.")
+print("3. 연산자 입력칸에 '=' 입력시 값이 출력됩니다.")
+print("4. 연산자 입력칸에 'AC' 입력시 값이 초기화 됩니다.")
+
 answer = 0
+
+while True:
+    n1 = int(input("숫자를 입력해주세요 : "))
+    shape = input("연산자를 입력해주세요 : ")
+
+    # 무한루프 종료 조건
+    if shape == "=":
+        print("[", answer, "]")
+        break
+    
+    if shape == "+":
+        pass
+    elif shape == "-":
+        pass
+    elif shape == "*":
+        pass
+    elif shape == "/":
+        pass
+    elif shape == "%":
+        pass
+    elif shape == "AC":
+        pass
+
 
